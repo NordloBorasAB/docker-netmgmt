ed9307b3aea719e64a2b23087dcbc47fcb75c761
invade3r <98033688+invade3r@users.noreply.github.com>
2026-06-25 19:56:44 +0200

    Update configuration.py
    Added named plugins but commented out

--- netbox/configuration/configuration.py
+++ netbox/configuration/configuration.py
@@ -256,7 +256,11 @@ if 'PAGINATE_COUNT' in environ:
     PAGINATE_COUNT = _environ_get_and_map('PAGINATE_COUNT', None, _AS_INT)
 
 # # Enable installed plugins. Add the name of each plugin to the list.
-# PLUGINS = []
+#PLUGINS = [
+#    'netbox_floorplan',
+#    'netbox_secrets',
+#    'netbox_topology_views',
+#]
 
 # # Plugins configuration settings. These settings are used by various plugins that the user may have installed.
 # # Each key in the dictionary is the name of an installed plugin and its value is a dictionary of settings.
